de29f70b7b8cd65846ac28500d8b26fc234393cc
alikhan-brac <alikhan.brac@gmail.com>
2023-07-12 13:24:23 +0600

    update

--- restapiWithAllure/src/test/java/qa/restapi/constants/ApiUrl.java
+++
@@ -1,9 +0,0 @@
-package qa.restapi.constants;
-
-import lombok.experimental.UtilityClass;
-import qa.restapi.util.JsonDataPuller;
-
-@UtilityClass
-public class ApiUrl {
-	public static final String BASE_URI = JsonDataPuller.dataPull(FilePaths.CONFIG_DATA, "/apiurl");
-}
--- restapiWithAllure/src/test/java/qa/restapi/constants/FilePaths.java
+++
@@ -1,9 +0,0 @@
-package qa.restapi.constants;
-
-import lombok.experimental.UtilityClass;
-
-@UtilityClass
-public class FilePaths {
-	public static final String CONFIG_DATA = ".\\src\\test\\resources\\config.json";
-	public static final String TEST_DATA = ".\\src\\test\\resources\\testData.json";
-}
